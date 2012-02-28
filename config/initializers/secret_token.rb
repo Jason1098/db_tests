7fcdf28f68957cef7af149c6853bf165c293cb36
jason1098 <jasonbushis@gmail.com>
2012-02-28 11:01:55 -0800

    rails framework added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbTests::Application.config.secret_token = '8b344121f1c5cbc78143b34f730033e7bb58d4ca29f0597b73b7c6141dd86725c35fa852709c4a0c958809cc1081f4ee5524d1f9051cab46becbda2541dab3b3'
