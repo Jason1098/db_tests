af89ac1f48401d0c496f0f8ef02af6c4a7911b2e
jason1098 <jasonbushis@gmail.com>
2012-02-29 17:01:11 -0800

    got the validate_timeliness to work again...  changing :on => :create to :on => :save doesn't work like i had hoped....  but it works with :on => :new

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 member1 = Member.create(first_name: 'Jason', last_name: 'Bush', gender: 'Alpha-Male', start_date: '01/01/2012', birthdate: '1972/04/19')
-member2 = Member.create(first_name: 'Shereef', last_name: 'Bishay', gender: 'Male', start_date: '01/02/2012', birthdate: '1977/mar/05')
-member3 = Member.create(first_name: 'Jolana', last_name: 'Bishay', gender: 'Female', start_date: '01/10/2012', birthdate: '1978/nov/19')
+member2 = Member.create(first_name: 'Shereef', last_name: 'Bishay', gender: 'Male', start_date: '01/02/2012', birthdate: '1977/03/05')
+member3 = Member.create(first_name: 'Jolana', last_name: 'Bishay', gender: 'Female', start_date: '01/10/2012', birthdate: '1978/11/19')
  
 team1 = Team.create(team_name: 'Super-Duper Butterfly Princess Faries', jersey_color: 'Pink and Purple', rating: 'very, very, very ok')
 team2 = Team.create(team_name: 'Pretty Wonderful Beautiful Angel Unicorns', jersey_color: 'Yellow and Pink', rating: 'extremely decent')
